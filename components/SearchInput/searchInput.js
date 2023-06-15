a4d97a3fcca87ebb804eaa1ec425b4fee89684ee
Sapna Meena <trivediheenalove@gmail.com>
2023-06-15 18:27:32 +0530

    handle search functionality for blogs

---
+++ components/SearchInput/searchInput.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { AiOutlineSearch } from 'react-icons/ai';
+import debounce from 'lodash/debounce';
+import { getSearchInputData } from "../../lib/api";
+import {
+    Input,
+} from "@chakra-ui/react";
+import { postPathBySlugCategory } from "../../lib/posts";
+
+export default function SearchBox(props) {
+    const [searchField, setSearchField] = useState([])
+    const [inputField, setInputField] = useState('')
+
+
+    const debouncedHandleSearch = debounce(async (searchValue) => {
+        console.log("searchval", searchValue)
+        const data = await getSearchInputData(searchValue);
+        setSearchField(data?.posts?.nodes)
+    }, 300);
+
+    function handleSearchInput(e) {
+        const inputValue = e.currentTarget.value;
+        setInputField(inputValue)
+        debouncedHandleSearch(inputValue);
+    }
+    return (
+        <div className="searchbox" style={{ width: props.width || "100%" }}>
+            <div style={{ background: "white", border: "1px solid white", borderRadius: "10px", display: "flex", alignItems: "center", padding: "0px 5px", height: "35px" }}>
+                <Input placeholder='Search...' borderRadius="10px" border="none" variant='flushed' w="100%" h="100%" value={inputField} style={{ outline: "none" }} onChange={((e) => handleSearchInput(e))} />
+                <AiOutlineSearch />
+            </div>
+            <div style={{ position: "absolute", background: "white", zIndex: "9999", width: "26%", borderRadius: "10px", marginTop: "10px" }}>
+                {searchField && searchField?.length !== 0 ? searchField?.map((item, i) => {
+                    let categorySlug = item?.categories?.edges[0]?.node?.slug
+
+                    return (<Link href={postPathBySlugCategory(
+                        item?.slug,
+                        categorySlug
+                    )} key={i} style={{ marginTop: "5px", padding: "5px 15px 8px" }}>
+                        <div style={{ display: "flex", alignItems: "center", outline: "none" }}>
+                            <img src={item?.featuredImage?.["node"]?.sourceUrl} style={{ width: "45px", height: "45px", borderRadius: "50%" }} alt="image" />
+                            <h4 style={{ padding: "0px 5px", color: "black" }}>{item.title}</h4>
+                        </div>
+                    </Link>)
+
+                }) : null}
+            </div>
+        </div>
+    )
+}
